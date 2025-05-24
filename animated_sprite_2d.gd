81a87647e7ea46421ec1e83879211fd14e6ba6d7
kyuna <kim_u_na@dgist.ac.kr>
2025-05-24 21:28:12 +0900

    moving add

--- animated_sprite_2d.gd
+++ animated_sprite_2d.gd
@@ -1,1 +1,20 @@
 extends AnimatedSprite2D
+
+var isattack = false
+
+func _ready() -> void:
+	play("default")
+
+func _process(delta: float) -> void:
+	if isattack != true:
+		play("default")
+
+func _input(event): 
+	if event.is_action_pressed("Enter"):
+		isattack = true
+		play("attack")
+	
+
+func _on_animation_finished() -> void:
+	if animation == "attack":
+		isattack = false
---
+++ cloud_2.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@export var interval = 0.1 # n초 (기본값 1초)
+var time_passed = 0.0  # 누적 시간
+
+func _process(delta):
+	time_passed += delta
+	
+	if time_passed >= interval:
+		position.x -= 0.05
+		time_passed = 0.0  # 초기화
